7eae99ba23f8dfd3350fc30403c7e98978751bbc
Avangard <info@dsavangard.ru>
2026-06-20 14:52:04 +0300

    test: bump label to #98

--- arted-cabinet-nav.php
+++ arted-cabinet-nav.php
@@ -175,7 +175,7 @@ function arted_tab_orders() {
     $t = [
         'ru' => [
             'title'      => 'Заказы',
-            'empty'      => 'Заказов пока нет #97',
+            'empty'      => 'Заказов пока нет #98',
             'col_date'   => 'Дата',
             'col_order'  => '№ заказа',
             'col_work'   => 'Работа',
